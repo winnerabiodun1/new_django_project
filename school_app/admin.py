b7cb04e4c88982b8d9aaac7368918b4a027bc218
Winner Abiodun <80297978+winnerabiodun1@users.noreply.github.com>
2021-05-21 10:16:26 +0100

    creating data in our database

--- school_app/admin.py
+++ school_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Student, School
 # Register your models here.
+
+admin.site.register(School)
+admin.site.register(Student)
--- school_app/models.py
+++ school_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class School (models.Model):
+    name = models.CharField(max_length=400)
+    address = models.CharField(max_length=400)
+    postal_code = models.IntegerField()
+
+
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    School = models.ForeignKey(School, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    age = models.IntegerField()
+    date_of_resumption = models.DateField(default=datetime.today)
+
+
+    def __str__(self):
+        return self.first_name
